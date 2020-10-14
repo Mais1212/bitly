7bcbd3e0dc76c80c802a16571dad19857d9c4711
Mais1212 <dragon_bord@mail.ru>
2020-10-14 21:16:43 +0300

    add README

--- main.py
+++ main.py
@@ -3,39 +3,47 @@ from dotenv import load_dotenv
 import requests
 import os
 
-def shorten_link(headers, url):
-	response = requests.post('https://api-ssl.bitly.com/v4/bitlinks', headers=headers, json=url)
-	shorten_link = response.json()
-	return shorten_link["link"]
 
-def count_clicks(headers, url):
-	url_urlparse = urlparse(url).path
+def shorten_link(headers, link):
+    response = requests.post(
+        'https://api-ssl.bitly.com/v4/bitlinks',
+        headers=headers,
+        json=link)
 
-	response = requests.get('https://api-ssl.bitly.com/v4/bitlinks/bit.ly{}/clicks/summary'.format(url_urlparse),
-	 headers=headers)
-	clicks_count = response.json()
-	return clicks_count["total_clicks"]
+    short_link = response.json()
+    return short_link["link"]
+
+
+def count_clicks(headers, long_link):
+    url_urlparse = urlparse(long_link).path
+
+    response = requests.get(
+        'https://api-ssl.bitly.com/v4/bitlinks/bit.ly{}/clicks/summary'.format(
+            url_urlparse),
+        headers=headers)
+    clicks_count = response.json()
+    return clicks_count["total_clicks"]
+
 
 if __name__ == '__main__':
-	url = input("Ссылка : ")
+    link = input("Ссылка : ")
 
-	load_dotenv()
-	token = os.getenv("TOKEN")
-	headers = {
-	    'Authorization': 'Bearer ' + str(token)
-	}
-	data = { "long_url": url}
+    load_dotenv()
+    token = os.getenv("TOKEN")
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
+    long_link = {"long_url": link}
 
-
-	if url.startswith("bit.ly", 8, 14):
-		try:
-			count_clicks = count_clicks(headers,url)
-			print("Количество кликов : " + str(count_clicks))
-		except LookupError:
-			print("Ошибка, введите ссылку повторно")
-	else :		
-		try:
-			bitlink = shorten_link(headers,data)
-			print('Битлинк : ' + bitlink)
-		except LookupError:
-			print("Ошибка, введите ссылку повторно")
+    if link.startswith("bit.ly", 8, 14):
+        try:
+            count_clicks = count_clicks(headers, link)
+            print("Количество кликов : " + str(count_clicks))
+        except LookupError:
+            print("Ошибка, введите ссылку повторно")
+    else:
+        try:
+            bitlink = shorten_link(headers, long_link)
+            print('Битлинк : ' + bitlink)
+        except LookupError:
+            print("Ошибка, введите ссылку повторно")
