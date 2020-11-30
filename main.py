f2e3c4041746af3294fe75f341483a2d64fac4ae
Mais1212 <dragon_bord@mail.ru>
2020-11-30 18:39:10 +0300

    Added new exceptions

--- main.py
+++ main.py
@@ -12,15 +12,17 @@ def shorten_link(headers, link):
         json=data)
 
     short_link = response.json()
+    print(short_link)
     return short_link["link"]
 
 
 def count_clicks(headers, long_link):
     url_urlparse = urlparse(long_link).path
     response = requests.get(
-        f'https://api-ssl.bitly.com/v4/bitlinks/bit.ly{url_urlparse}\
+        f'https://api-ssl.bitly.com/v4/bitlinks/{url_urlparse}\
         /clicks/summary',
         headers=headers)
+    print(url_urlparse)
     clicks_count = response.json()
     return clicks_count["total_clicks"]
 
@@ -35,11 +37,15 @@ def main():
     }
 
     try:
-        counted_clicks = (count_clicks(headers, link))
-        print("Количество кликов : " + str(counted_clicks))
-    except Exception:
-        bitlink = shorten_link(headers, link)
-        print('Битлинк : ' + bitlink)
+        try:
+            counted_clicks = (count_clicks(headers, link))
+            print("Количество кликов : " + str(counted_clicks))
+        except KeyError:
+            bitlink = shorten_link(headers, link)
+            print('Битлинк : ' + bitlink)
+    except:
+        print("Введите ссылку повторно!")
+        main()
 
 
 if __name__ == '__main__':
